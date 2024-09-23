708b6342f879f930e596411893f873c532a8ea0b
ZacharyHolt2004 <ztholt2004@gmail.com>
2024-09-22 23:16:57 -0400

    GasBurner class added

---
+++ docs/ClassDiagrams/GasBurner.java
@@ -0,0 +1,26 @@
+public class GasBurner extends Burner{
+    //fields
+    private String gasType;
+
+
+    public GasBurner(){
+
+    }
+
+    public boolean spark(){
+        return false;
+    }
+
+    public String getGasType(){
+
+    }
+
+    private void increaseGasFlow(){
+
+    }
+
+    private void decreaseGasFlow(){
+
+    }
+
+}
