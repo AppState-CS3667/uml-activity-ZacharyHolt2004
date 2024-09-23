3ca56b38dcbb43fb78a1af9eef67a38acb9912da
ZacharyHolt2004 <ztholt2004@gmail.com>
2024-09-22 23:14:33 -0400

    ElectricBurner frame added

---
+++ docs/ClassDiagrams/ElectricBurner.java
@@ -0,0 +1,21 @@
+public class ElectricBurner extends Burner{
+    //fields
+    private voltReq double;
+    private ampReq double;
+
+    public ElectricBurner(double voltReq){
+
+    }
+
+    public void increaseCurrent(){
+
+    }
+
+    public void decreaseCurrent(){
+
+    }
+
+    public double getVoltReq(){
+        return -1;
+    }
+}
