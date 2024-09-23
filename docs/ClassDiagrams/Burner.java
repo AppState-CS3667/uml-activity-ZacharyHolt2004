9301001b7040a7152c7ce8bbaeb489ab9718c417
ZacharyHolt2004 <ztholt2004@gmail.com>
2024-09-22 23:11:19 -0400

    Burner class frame added

---
+++ docs/ClassDiagrams/Burner.java
@@ -0,0 +1,44 @@
+public class Burner{
+    //Fields
+    private double diameter;
+    private double maxTemp;
+    private double minTemp;
+    private boolean isHot;
+
+    public Burner(double maxTemp, double minTemp, double diameter){
+
+    }
+
+    public boolean turnOn(){
+        return false;
+    }
+
+    public boolean turnOff(){
+        return false;
+    }
+
+    public void increaseTemp(){
+
+    }
+
+    public void decreaseTemp(){
+
+    }
+
+    public double getDiameter(){
+
+    }
+
+    public double getMaxTemp(){
+
+    }
+
+    public double getMinTemp(){
+
+    }
+
+    public boolean isHot(){
+        return false;
+    }
+
+}
